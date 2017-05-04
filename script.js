883ffbbdcb4aadf293a13c6efe493f33a23ee36e
Jason Occhipinti <positivespacegroup@gmail.com>
2017-05-04 00:04:45 -0400

    Styles, SASS/CSS
    Button styles, formatting, SASS

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ navigation = function() {
       transitionSpeed: 250,           // Number:    Specify the transition speed of the menu
       transitionType: 'slide',        // String:    Specify the type of menu transition
       location: 'top',                // String:    Specify the location of the menu's origin
-      menuToggle: "#navBtn",           // Selector:  Specify the ID of the toggle element
-      menuID: "#navMenu",              // Selector:  Specify the ID of the main menu element
-      navbarID: "#navBar",             // Selector:  Specify the ID of the navigation bar
+      menuToggle: "#navBtn",          // Selector:  Specify the ID of the toggle element
+      menuID: "#navMenu",             // Selector:  Specify the ID of the main menu element
+      navbarID: "#navBar",            // Selector:  Specify the ID of the navigation bar
       container: "#main"
     };
 
@@ -27,12 +27,14 @@ navigation = function() {
 
   var menu  = document.querySelector(defaults.menuID);
   var main  = document.querySelector(defaults.container);
-      //btn   = document.querySelector(defaults.menuToggle);
+  var btn   = document.querySelector(defaults.menuToggle);
       //nabar = document.querySelector(defaults.navbarID);
 
 function initNavigation() {
   main.classList.add('closed');
   menu.classList.add('closed');
+  btn.classList.add('closed');
+
   //menu.classList.add(defaults.transitionSpeed);
   menu.setAttribute("style", "speed:" + defaults.transitionSpeed);
 
@@ -44,10 +46,12 @@ function initNavigation() {
       menu.classList.toggle('open');
       main.classList.toggle('closed');
       main.classList.toggle('open');
+      btn.classList.toggle('closed');
+      btn.classList.toggle('open');
     }
 
 
-    document.querySelector('a#navBtn').addEventListener('click', b );
+    document.querySelector('#navBtn').addEventListener('click', b );
 
 
 }
